0e19ff2d3200ab2011a702084505d78658010513
Alexey Pyatov <alexey.pyatov@sas.com>
2021-12-25 22:13:26 +0300

    211225 upd

--- src/storybuilder.py
+++ src/storybuilder.py
@@ -1,101 +1,33 @@
-import re
-import yaml_handler
+def parse_story_md(story_md_file: str) -> dict:
+    parsed_story = ""
+    return parsed_story
 
-def md_read (md_file: str) -> list:
-    with open(f'{md_file}','r', encoding="utf-8") as md_file:
-        # md = list(md_file) # это если пофиг на \n, но нам надо убрать
-        # убираем \n в конце строк и пустые строки из файла
-        md = [x.rstrip() for x in md_file]
-        md_strip = [x for x in md if x != '']
-    return md_strip
-
-def md_parse (md_list: list) -> list:
-
-    out_list = list()
-    rownum = 0
-    level_index = 1
-
-    while rownum < len(md_list):
-
-        # ищем заголовки
-        match = re.search('(#+)(.*)', md_list[rownum])
-        if match:
-            level = len(match.group(1))
-            section_key = f'H{level}_{level_index}'
-            section_label = match.group(2).strip()
-
-            rownum = rownum + 1
-            start_rownum = rownum
-
-            below = md_list [start_rownum:]
-
-            for x in range(len(below)):
-
-                # ищем диапазон строк с содержимым секции
-                match_below = re.search('(#+)(.*)', below[x])
-                if match_below:
-                    level_below = len(match_below.group(1).strip())
-                    if level_below <= level:
-                        break
-                    else:
-                        rownum = rownum + 1
-                else:
-                    rownum = rownum + 1
-            end_rownum = rownum
-            # нашли номер строки, где заканчивается секция (либо дошли до конца)
-
-            section_content = md_parse (md_list[start_rownum:end_rownum])
-            out_list.append ({section_key: {'label': section_label, 'content': section_content}})
-            level_index = level_index + 1
-
-        else:
-            out_list.append (md_list[rownum])
-            rownum = rownum + 1
-
-    return out_list
-
-def scenes_outline_builder (md_list: list) -> list:
-
-    # выбрасываем комменты
-    # match_comment = re.search('(//)(.*)', md_list[rownum])
-    # if not match_comment:
-    #     ...
-
-    return md_list
-
-
-def get_scenes(content: list) -> list:
+def collect_scenes(parsed_story: dict) -> list:
     scenes = []
-    scenes_src = [x for x in content] # dicts (each is heading and below)
-    for item in scenes_src:
-        print (item)
-        # scenes.append()
-
-    scenes=scenes_src
     return scenes
+
+def concatenate_scenes(scenes: list) -> str:
+    catx_scenes = "\n".join(scenes)
+    return catx_scenes
+
+def write_full_story_to_file(story: str, filename: str) -> bool:
+    hey = type(story)
+    print (f"writing story {hey} to {filename}")
+    return True
 
 def main():
 
-    # md2 = ['## Scene 1','Scene 1 Text','Scene 1 More Text', '## Scene 2','Scene 2 Text', '### Subscene 1' ,'Subscene 1 Text', '## Scene 3', 'Scene 3 Text']
-    # md_in = md2
+    story_md_file = "story.md"
+    filename = "story_full.md"
 
-    md_file = 'src/story.md'
-    md_in = md_read (md_file)
+    parsed_story = parse_story_md(story_md_file)
+    scenes = collect_scenes(parsed_story)
+    formatted_story = concatenate_scenes(scenes)
+    write_full_story_to_file(formatted_story, filename)
 
-    # print ('in:')
-    # print (md_in)
+    print (f"Done! Story was successfully built and written into {filename}")
 
-    md_parsed = md_parse (md_in)
+    return True
 
-    # yaml_handler.yaml_write('meta/story.yaml',md_parsed)
-
-    content=md_parsed[0]['H1_1']['content']
-    for item in content:
-        val = [x for x in item.values()][0]
-        if val['label'] == 'Scenes':
-            scenes = get_scenes(val['content'])
-            # for scene in scenes:
-            #     print (scene)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
