d2598cb10f9d5850bbb727cf17567958917dec6d
Alexey Pyatov <a.a.pyatov@gmail.com>
2022-12-07 23:27:40 +0300

    refactoring

--- storybuilder/core/storybuilder.py
+++ storybuilder/core/storybuilder.py
@@ -18,7 +18,6 @@ def scene_text_writer (file_TextIOWrapper, scene_text: list):
 
 def main () -> int:
 
-    # время начала обработки
     start_datetime = datetime.now().strftime("%y%m%d_%H-%M-%S")
 
     in_file, out_file, debug_mode_on = get_args()
@@ -34,33 +33,41 @@ def main () -> int:
 
     print (f"Parsing outline from {outline_file_abs}")
 
-    outline_content = get_md_file_content (outline_file_abs)
+    outline_content = get_md_file_content(outline_file_abs)
 
     with open(out_file_abs,'w', encoding="utf-8") as out_file:
         for line in tqdm(outline_content):
             title_match = re.search('^\s*(#+)(.*)', line)
+            md_url_match = re.search("\[.+\]\((.+)\)", line)
+            not_finished_scene_match = re.search("^\s*-\s*(.+)$", line)
+            comment_match = re.search("^\s*//\s*(.*)", line)
+
+            # пишем заголовки as is, только для режима отладки
+            # добавим пометку в начало файла
             if title_match:
-                if title_match.group(1) == "#" and debug_mode_on:
-                    out_file.write(line + " // DEBUG_MODE\n\n")
-                else:
-                    out_file.write(line + "\n\n")
+                debug_title = "// DEBUG_MODE" if title_match.group(1) == "#" and debug_mode_on else ""
+                out_file.write(f"{line}{debug_title}\n\n")
+
+            # сцены забираем из файлов по ссылке, парсим
+            # и выводим второй блок (Text) с текстом сцены
+            elif md_url_match:
+                if debug_mode_on:
+                    out_file.write(f"=== {md_url_match.group(0)} ===\n\n")
+                md_url = md_url_match.group(1)
+                scene_filename = os.path.abspath(f'{in_folder}/{md_url}')
+                scene_file_content = get_md_file_content(scene_filename)
+                scene_file_content_yaml = parse_md_yaml(scene_file_content)
+                scene_text = scene_file_content_yaml[0]["H1_1"]["content"]\
+                                                    [1]["H2_2"]["content"]
+                scene_text_writer(out_file, scene_text)
+
+            # названия незавершенных сцен выводим как есть (без - в начале)
+            elif not_finished_scene_match:
+                    out_file.write(f"=== {not_finished_scene_match.group(1)}\n\n")
+
+            # все остальное нам не нужно (комментарии, например)
             else:
-                md_url_match = re.search('^\s*-\s*\[.+\]\((.+)\)', line)
-                if md_url_match:
-                    if debug_mode_on:
-                        out_file.write(f"--{md_url_match.group(0)}\n\n")
-                    md_url = md_url_match.group(1)
-                    scene_filename = os.path.abspath(f'{in_folder}/{md_url}')
-                    scene_file_content = get_md_file_content(scene_filename)
-                    scene_file_content_yaml = parse_md_yaml(scene_file_content)
-                    scene_text = scene_file_content_yaml[0]["H1_1"]["content"]\
-                                                        [1]["H2_2"]["content"]
-                    scene_text_writer(out_file, scene_text)
-                else:
-                    comment_match = re.search("^\s*//\s*(.*)", line)
-                    not_finished_scene_match = re.search("^\s*-\s*(.+)$", line)
-                    if not comment_match and not_finished_scene_match:
-                        out_file.write(f"--- {not_finished_scene_match.group(1)}\n\n")
+                pass
 
     print (f"Done. Story was successfully built and written to {out_file_abs}")
     return 0
